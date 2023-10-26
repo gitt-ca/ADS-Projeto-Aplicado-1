d4ca2420448e647bf65a7f7c6c2f837da0dfcf85
VitorAlbara <92799928+VitorAlbara@users.noreply.github.com>
2023-10-26 20:41:16 -0300

    11

--- HFC/src/gui/Tela.java
+++ HFC/src/gui/Tela.java
@@ -1,4 +1,5 @@
 package gui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -6,77 +7,77 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 
 public class Tela {
-    private JFrame frame;
-    private JPanel mainPanel;
-    private JPanel menuPanel;
-    private JButton button1;
-    private JButton button2;
+	private JFrame frame;
+	private JPanel mainPanel;
+	private JPanel menuPanel;
+	private JButton button1;
+	private JButton button2;
 
-    public Tela() {
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.getContentPane().setLayout(new BorderLayout());
+	public Tela() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.getContentPane().setLayout(new BorderLayout());
 
-        createMenuPanel();
-        createMainPanel();
-        addHealthImage();
-        frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
-        menuPanel = new JPanel();
-        mainPanel.add(menuPanel, BorderLayout.WEST);
-        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
-        menuPanel.setBackground(new Color(101, 153, 255)); // Azul
-        
-                button1 = new JButton("Opção 1");
-                button2 = new JButton("Opção 2");
-                
-                        button1.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent e) {
-                                // Ação para a Opção 1
-                            }
-                        });
-                        
-                                button2.addActionListener(new ActionListener() {
-                                    @Override
-                                    public void actionPerformed(ActionEvent e) {
-                                        // Ação para a Opção 2
-                                    }
-                                });
-                                
-                                        menuPanel.add(Box.createRigidArea(new Dimension(0, 20))); // Espaçamento
-                                        menuPanel.add(button1);
-                                        menuPanel.add(button2);
+		createMenuPanel();
+		createMainPanel();
+		addHealthImage();
+		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+		menuPanel = new JPanel();
+		mainPanel.add(menuPanel, BorderLayout.WEST);
+		menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
+		menuPanel.setBackground(new Color(51, 153, 255)); // Azul
 
-        frame.setVisible(true);
-    }
+		button1 = new JButton("Opção 1");
+		button2 = new JButton("Opção 2");
 
-    private void createMenuPanel() {
-    }
+		button1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Ação para a Opção 1
+			}
+		});
 
-    private void createMainPanel() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
+		button2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Ação para a Opção 2
+			}
+		});
 
-        JLabel titleLabel = new JLabel("HealthFamilyCenter");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		menuPanel.add(Box.createRigidArea(new Dimension(0, 20))); // Espaçamento
+		menuPanel.add(button1);
+		menuPanel.add(button2);
 
-        mainPanel.add(titleLabel, BorderLayout.NORTH);
-    }
+		frame.setVisible(true);
+	}
 
-    private void addHealthImage() {
-        ImageIcon healthImage = new ImageIcon("health_icon.png"); // Insira o caminho da imagem
-        JLabel imageLabel = new JLabel(healthImage);
-        mainPanel.add(imageLabel, BorderLayout.EAST);
-    }
+	private void createMenuPanel() {
+	}
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Tela();
-            }
-        });
-    }
+	private void createMainPanel() {
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+
+		JLabel titleLabel = new JLabel("HealthFamilyCenter");
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+
+		mainPanel.add(titleLabel, BorderLayout.NORTH);
+	}
+
+	private void addHealthImage() {
+		ImageIcon healthImage = new ImageIcon("health_icon.png"); // Insira o caminho da imagem
+		JLabel imageLabel = new JLabel(healthImage);
+		mainPanel.add(imageLabel, BorderLayout.EAST);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Tela();
+			}
+		});
+	}
 }
